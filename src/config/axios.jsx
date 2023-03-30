37c5beb21f4bbe3b3cd5e61c982d3566a26eb98a
Nicolas <nicobenpe@gmail.com>
2023-03-29 21:18:47 -0300

    private routes con autenticidad

--- src/config/axios.jsx
+++ src/config/axios.jsx
@@ -4,9 +4,26 @@ const axios = axiosOrigin.create({
     baseURL: "http://localhost:4500",
 })
 
-// axios.interceptors.response(()=> {
+// axios.interceptors.request((config)=> {
+//     const token = localStorage.getItem("token");
 
+//     if(token) config.headers.authorization = tokenM
+//     return config
+// },  
+//     (error) => {
+//         return Promise.reject(error)
+//     }
 
+// )
+
+// axios.interceptors.response(null, (error) => {
+//     const{status} = error.response
+//     switch(status){
+//         case "404": navigate("/404"); break;
+//         case "401": navigate("/login"); break;
+//         case "403": navigate("/home");
+//     }
 // })
+
 
 export default axios
---
+++ src/context/UserContext.jsx
@@ -0,0 +1,8 @@
+// import { createContext, useState } from "react";
+
+// const UserContext = createContext();
+
+// const UserProvider = ({children}) =>{
+//     const [user, setUser] = useState(null);
+//     const [autentico, setAutentico]
+// } 
